3d9db3439efaea408419a99a7b628b99f30c07d6
Swastik Bhattacharyya <81484438+SwastikBhattacharyya@users.noreply.github.com>
2023-10-28 13:11:12 +0530

    Merge pull request #2
    This pull request adds the view models 'AccountViewModel' and 'ChatViewModel' classes. The 'AccountViewModel' class contains an 'open_chat_page' function which opens the 'ChatView' and the 'ChatViewModel' contains functions to create the chat boxes for sending and receiving messages. The functions and variables in both the view models have been bounded to the respective views.

--- src/pages/views/account.py
+++ src/pages/views/account.py
@@ -1,4 +1,5 @@
 from customtkinter import CTk, CTkLabel, CTkTabview, CTkEntry, CTkButton
+from src.pages.view_models.account import AccountViewModel
 
 
 class AccountView(CTk):
@@ -8,6 +9,8 @@ class AccountView(CTk):
         self.geometry('500x500')
         self.resizable(False, False)
         self.configure(fg_color='#535c68')
+
+        self.view_model: AccountViewModel = AccountViewModel(self)
 
         self.header: CTkLabel = CTkLabel(self, text='ChatArtifico', font=('Roboto', 24), text_color='#dff9fb')
         self.header.pack(pady=(20, 10))
@@ -23,33 +26,37 @@ class AccountView(CTk):
                                                     text_color='#c7ecee')
         self.sing_in_user_name.pack(padx=10, pady=5, anchor='nw')
 
-        self.sing_in_user_name_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign In'), font=('Roboto', 15))
+        self.sing_in_user_name_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign In'), font=('Roboto', 15),
+                                                          textvariable=self.view_model.sign_in_user_name)
         self.sing_in_user_name_entry.pack(padx=10, pady=5, anchor='nw', fill='x')
 
         self.sing_in_password: CTkLabel = CTkLabel(self.tab_view.tab('Sign In'), text='Password', font=('Roboto', 20),
                                                    text_color='#c7ecee')
         self.sing_in_password.pack(padx=10, pady=(20, 5), anchor='nw')
 
-        self.sing_in_password_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign In'), font=('Roboto', 15), show='*')
+        self.sing_in_password_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign In'), font=('Roboto', 15), show='*',
+                                                         textvariable=self.view_model.sign_in_password)
         self.sing_in_password_entry.pack(padx=10, pady=5, anchor='nw', fill='x')
 
         self.sing_in_button: CTkButton = CTkButton(self.tab_view.tab('Sign In'), text='Sign In', font=('Roboto', 15),
                                                    fg_color='#7ed6df', hover_color='#22a6b3', text_color='#130f40',
-                                                   height=40)
+                                                   height=40, command=self.view_model.open_chat_page)
         self.sing_in_button.pack(padx=10, pady=(20, 5), expand=True, fill='x')
 
         self.sing_up_user_name: CTkLabel = CTkLabel(self.tab_view.tab('Sign Up'), text='User Name', font=('Roboto', 20),
                                                     text_color='#c7ecee')
         self.sing_up_user_name.pack(padx=10, pady=5, anchor='nw')
 
-        self.sing_up_user_name_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign Up'), font=('Roboto', 15))
+        self.sing_up_user_name_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign Up'), font=('Roboto', 15),
+                                                          textvariable=self.view_model.sign_up_user_name)
         self.sing_up_user_name_entry.pack(padx=10, pady=5, anchor='nw', fill='x')
 
         self.sing_up_password: CTkLabel = CTkLabel(self.tab_view.tab('Sign Up'), text='Password', font=('Roboto', 20),
                                                    text_color='#c7ecee')
         self.sing_up_password.pack(padx=10, pady=(20, 5), anchor='nw')
 
-        self.sing_up_password_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign Up'), font=('Roboto', 15), show='*')
+        self.sing_up_password_entry: CTkEntry = CTkEntry(self.tab_view.tab('Sign Up'), font=('Roboto', 15), show='*',
+                                                         textvariable=self.view_model.sign_up_password)
         self.sing_up_password_entry.pack(padx=10, pady=5, anchor='nw', fill='x')
 
         self.sing_up_button: CTkButton = CTkButton(self.tab_view.tab('Sign Up'), text='Sign Up', font=('Roboto', 15),
