66c4c2dfb9556bf799f03d095968d980e72ffc6c
Swastik Bhattacharyya <swastikunity@gmail.com>
2023-10-28 19:06:13 +0530

    Integrate 'Sockets' module with 'ChatViewModel'
    This commit integrates the 'Sockets' module with the 'ChatViewModel class'. When a 'ChatView' instance is initialized, the client port and target port is prompted through the command line. The send function of the 'sockets' client is bound to the 'typing_button' command, while the 'receive' function from the sockets is delegated to two functions in the 'ChatViewModel' responsible for creating the chat boxes with the message for the sender or the recipient.

--- src/sockets/client.py
+++ src/sockets/client.py
@@ -1,44 +1,57 @@
 from socket import socket, AF_INET, SOCK_STREAM, error
 from threading import Thread
-from json import dumps
+from json import dumps, loads
+from typing import Callable
 
 
-PORT = int(input('Enter the port: '))
-TARGET_PORT: int = int(input('Enter the target port: '))
-HOST = '127.0.0.1'
+PORT: int | None = None
+TARGET_PORT: int | None = None
+HOST = None
+client: socket | None = None
 
-client: socket = socket(AF_INET, SOCK_STREAM)
-client.bind((HOST, PORT))
-client.connect((HOST, 5000))
+RECEIVE_MESSAGE_DELEGATE_TARGET: Callable[[str], None] | None = None
+RECEIVE_MESSAGE_DELEGATE_CLIENT: Callable[[str], None] | None = None
 
 
 def receive() -> None:
     while True:
         try:
             message = client.recv(1024)
-            print(message.decode('utf-8'))
+            message_dict = loads(message.decode('utf-8'))
+            if message_dict['target_port'] == PORT:
+                RECEIVE_MESSAGE_DELEGATE_TARGET(message_dict['message'])
+            else:
+                RECEIVE_MESSAGE_DELEGATE_CLIENT(message_dict['message'])
         except error:
-            print('An error occurred!')
             client.close()
             break
 
 
-def send() -> None:
-    while True:
-        message = input()
+def send(message: str) -> None:
+    message_dict = {
+        'sender_port': PORT,
+        'target_port': TARGET_PORT,
+        'message': message
+    }
+    message = dumps(message_dict)
 
-        message_dict = {
-            'sender_port': PORT,
-            'target_port': TARGET_PORT,
-            'message': message
-        }
-        message = dumps(message_dict)
-
-        client.send(message.encode('utf-8'))
+    client.send(message.encode('utf-8'))
 
 
-if __name__ == '__main__':
+def start(receive_message_delegate_target: Callable[[str], None],
+          receive_message_delegate_client: Callable[[str], None]) -> None:
+    global PORT, TARGET_PORT, HOST, client, RECEIVE_MESSAGE_DELEGATE_TARGET, RECEIVE_MESSAGE_DELEGATE_CLIENT
+
+    RECEIVE_MESSAGE_DELEGATE_TARGET = receive_message_delegate_target
+    RECEIVE_MESSAGE_DELEGATE_CLIENT = receive_message_delegate_client
+
+    PORT = int(input('Enter the port: '))
+    TARGET_PORT = int(input('Enter the target port: '))
+    HOST = '127.0.0.1'
+
+    client = socket(AF_INET, SOCK_STREAM)
+    client.bind((HOST, PORT))
+    client.connect((HOST, 5000))
+
     thread_receive = Thread(target=receive)
-    thread_send = Thread(target=send)
     thread_receive.start()
-    thread_send.start()
