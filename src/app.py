1eea4143fb5678ab219acf26ee4c4243398e4570
Swastik Bhattacharyya <swastikunity@gmail.com>
2023-10-28 13:19:59 +0530

    Refactor 'App' module to run GUI in another thread
    This commit refactors the 'App' module to run the 'mainloop' function on the instance of 'AccountView' in another thread. This will allow the 'client' module of the socket to be created later to run on another thread as well.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
 from src.pages.views.account import AccountView
+from threading import Thread
+
+
+def app():
+    AccountView().mainloop()
 
 
 if __name__ == "__main__":
-    AccountView().mainloop()
+    Thread(target=app).start()
